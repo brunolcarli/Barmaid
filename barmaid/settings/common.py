658c6fcd423ecc52cceac7e42613f2357db6b02b
Bruno L. Carli <brunolcarli@gmail.com>
2021-10-21 00:49:28 -0300

    ADD common settings

---
+++ barmaid/settings/common.py
@@ -0,0 +1,16 @@
+"""
+Main common system settings
+"""
+from decouple import config
+
+
+__version__ = '0.0.0'
+
+ENV_REF = config('ENV_REF', 'development')
+TOKEN = config('TOKEN')
+MAIN_CHANNEL = config('MAIN_CHANNEL')
+
+MYSQL_HOST = config('MYSQL_HOST', 'localhost')
+MYSQL_USER = config('MYSQL_USER', 'mysql_user')
+MYSQL_PASSWORD = config('MYSQL_PASSWORD', 'db_pwd')
+MYSQL_DATABASE = config('MYSQL_DATABSE', 'db_name')
