f782a4b749188265e22c8c06dbd005aaa81689fd
Bruno L. Carli <brunolcarli@gmail.com>
2021-10-22 15:02:29 -0300

    ADD get build discord_id util

--- core/util.py
+++ core/util.py
@@ -2,3 +2,7 @@
 
 def condition(column, value, operator='='):
     return {'column': column, 'operator': operator, 'value': value}
+
+
+def get_discord_id(server_id, author_id):
+    return f'{server_id}:{author_id}'
