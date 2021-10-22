6b155f9d4257ef2a9e11acb04849b1da4766315b
Bruno L. Carli <brunolcarli@gmail.com>
2021-10-21 22:02:35 -0300

    ADD increase coins on sending message, view funds command

--- core/commands.py
+++ core/commands.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 
-from barmaid.settings.common import (MAIN_CHANNEL, __version__)
-
+from barmaid.settings.common import (MAIN_CHANNEL, __version__, COIN_INCREASE_VALUE)
+from core.models import User
 
 
 client = commands.Bot(command_prefix='>')
@@ -31,9 +31,52 @@ async def on_ready():
     print("System loaded and running!")
 
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await client.process_commands(message)
+
+    server = message.guild.id
+    author = message.author.id
+    discord_id = f'{server}:{author}'
+
+    user = User(discord_id)
+    user.add_coin(COIN_INCREASE_VALUE)
+    print(f'User {discord_id} earned {COIN_INCREASE_VALUE} coins!')
+
+
 @client.command()
 async def version(ctx):
     """
     Pings the bot to return its current version.
     """
     await ctx.send(f'Running version: {__version__}')
+
+
+@client.command(aliases=['fds', 'coins', 'wallet'])
+async def funds(ctx):
+    """
+    Checks your current coin amount.
+    """
+    server = ctx.guild.id
+    author = ctx.author.id
+
+    discord_id = f'{server}:{author}'
+    user = User(discord_id)
+    coins = int(user.coins)
+
+    label = 'coin' if coins == 1 else 'coins'
+
+    embed = discord.Embed(color=0x1E1E1E, type='rich')
+    embed.add_field(name='Name:', value=ctx.author.name, inline=False)
+    embed.add_field(
+        name='Your balance:',
+        value=f':coin: : {coins} {label}',
+        inline=True
+    )
+    avatar = f'{ctx.author.avatar_url.BASE}{ctx.author.avatar_url._url}'
+    embed.set_thumbnail(url=avatar)
+
+    await ctx.send('Funds', embed=embed)
