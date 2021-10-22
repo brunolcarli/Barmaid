b975e4580fbfb68389ebe1d180856a01f5c30e26
Bruno L. Carli <brunolcarli@gmail.com>
2021-10-22 15:01:22 -0300

    ADD item models

--- core/models.py
+++ core/models.py
@@ -3,6 +3,12 @@ from core.util import condition
 
 
 class User:
+    """
+    Representation of an user object on the system.
+    Display and manipulate user attribute values as well as handling the
+    object interactions such as transactions and data storing and retrieving
+    from database.
+    """
     def __init__(self, discord_id):
         # resove data from db
         user = get_or_create_user(discord_id)
@@ -12,6 +18,7 @@ class User:
         )
         purchases = next(iter(purchases), None)
 
+        # Defines object attributes
         self.id = user[0]
         self.discord_id = discord_id
         self.coins = user[2]
@@ -35,3 +42,37 @@ class User:
     def add_coin(self, value):
         self.coins += value
         self.save()
+
+
+class Item:
+    def __init__(self, data):
+        self.code = data[0]
+        self.name = data[1]
+        self.value = data[2]
+        self.description = data[3]
+        self.sell_count = data[4]
+        self.sprite = f'static/img/{data[5]}'
+
+    def __repr__(self) -> str:
+        return self.name
+
+
+class SpecificItem(Item):
+    def __init__(self, code):
+        item = read_query(
+            create_db_connection(),
+            DBQueries.select_item(where=condition('item_id', code))
+        )
+        if not item:
+            raise NameError(f'Item {code} does not exist!')
+
+        super().__init__(next(iter(item)))
+
+
+class ItemList:
+    def __init__(self):
+        items = read_query(
+            create_db_connection(),
+            DBQueries.select_item()
+        )
+        self.items = [Item(item) for item in items]
